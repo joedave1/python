b432ea66c3ecaa437ed4b8f5bd01f038ad1eb11e
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-05 00:42:06 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+c=int(input("Enter c: "))
+if a>b and a>c:
+    print("a is the greatest")
+elif b>c:
+    print("b is the greatest")
+else:
+    print("c is the greatest")
