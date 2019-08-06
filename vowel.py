42951c49ba3848157e44c1779706d9f9e736a435
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-06 13:00:22 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,13 +1,9 @@
-str1=str(input("Enter a letter: "))
-if str1=="a":
-  print("it's a vowel")
-elif str1=="e":
-  print("it's a vowel")
-elif str1=="i":
-  print("it's a vowel")
-elif str1=="o":
-  print("it's a vowel")
-elif str1=="u":
-  print("it's a vowel")
-else:
-  print("it's not a vowel")
+list1=['a','e','o','i','u']
+for i in list1:
+    x=input("Enter the alphabet")
+    if i==x:
+        print("vowel")
+        break
+    else:
+        print("Not a vowel")
+        break
