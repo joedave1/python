73d439aabf618df4ce13305579e78e70e3dc4935
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-07 19:55:08 -0900

    Create deletionintuple.py

---
+++ deletionintuple.py
@@ -0,0 +1,10 @@
+tup=(1,2,3,4,5,6)
+print("before deletions",tup)
+tup=tup[0:2]+tup[3:]
+print("after deletion",tup)tup=(1,2,3,4,5,6)
+print("before deletions",tup)
+tup=tup[0:2]+tup[3:]
+print("after deletion",tup)tup=(1,2,3,4,5,6)
+print("before deletions",tup)
+tup=tup[0:2]+tup[3:]
+print("after deletion",tup)
