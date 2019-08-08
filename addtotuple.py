1ad053dd78229b15690a08db76d52fe4ebbe1dc5
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-07 19:55:34 -0900

    Create addtotuple.py

---
+++ addtotuple.py
@@ -0,0 +1,7 @@
+tup=(1,2,3,4)
+print("original tuple",tup)
+tup=(1,2,3,4)+(5,6)
+print("modified tuple", tup)tup=(1,2,3,4)
+print("original tuple",tup)
+tup=(1,2,3,4)+(5,6)
+print("modified tuple", tup)
