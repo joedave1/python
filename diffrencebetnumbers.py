ebdb576cf7d5d695b3da40d87c2a42b970123040
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-07 19:51:21 -0900

    Create diffrencebetnumbers.py

---
+++ diffrencebetnumbers.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number: "))
+if n<17:
+    ans=17-n    
+elif n>17:
+    ans=2*(n-17)
+print("The difference is ",ans)n=int(input("Enter a number: "))
+if n<17:
+    ans=17-n    
+elif n>17:
+    ans=2*(n-17)
+print("The difference is ",ans)
