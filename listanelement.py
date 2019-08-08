1f5b87bc2b0f07ab5cec171db9a49d8e0bd13c34
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-07 19:50:05 -0900

    Create listanelement.py

---
+++ listanelement.py
@@ -0,0 +1,5 @@
+x=input("Enter a commc seperated list values: ").split(",")
+color=list(x)
+print("The first color is %s and the last color is %s"%(color[0],color[-1]))x=input("Enter a commc seperated list values: ").split(",")
+color=list(x)
+print("The first color is %s and the last color is %s"%(color[0],color[-1]))
