9d1fde45a2a952f24b925f37d50ff9414ef40d28
joedave1 <53596234+joedave1@users.noreply.github.com>
2019-08-11 22:15:47 +0530

    Create listofprimes.py

---
+++ listofprimes.py
@@ -0,0 +1,13 @@
+l = int(input("Enter starting range: "))
+u = int(input("Enter ending range: "))
+
+print("Prime numbers between",l,"and",u,"are:")
+
+for num in range(l,u+1):
+  
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
